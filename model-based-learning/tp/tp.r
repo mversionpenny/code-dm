cc45d279eaad4784ae4cbfa9c7c4a5311a86ac7a
mversionpenny <margot.selosse@gmail.com>
2016-11-21 15:52:15 +0100

    model-based td first shot ( with visa premier)

--- model-based-learning/tp/tp.r
+++ model-based-learning/tp/tp.r
@@ -17,16 +17,89 @@ ncol(data) # 48
 #### First clusters with mclust ####
 # ncol = 48, it's a bit much! --> TODO : maybe think about pca or lasso later
 model <- model.matrix(~.+0, data=data)
-# now, model has 212 dimensions!!
+# now, model has 212 dimensions!! --> dummy variables
 BIC <- mclustBIC(model) # takes a lot of time
 plot(BIC) 
 summary(BIC)
 
+#### "Cleaning" data ####
+
 # There are too much dimensions, we want to get rid of some of them
 # After a reading of the description, we see that we can manually get rid of 
 # cartevp : because it's like cartvpr
+# sexe : because it's like sexer
+idx_sexe <- which(colnames(data)=="sexe")
+idx_cartevp <- which(colnames(data)=="cartevp")
+data <- data[,-c(idx_sexe,idx_cartevp)]
+model <- model.matrix(~.+0, data=data)
+dim(model) # still 210 --> too much
 
+# We note that nbimpaye is always 0
+sum(which(data$nbimpaye !=0))
+# se can delete this column
+idx_nbimpaye <- which(colnames(data)=="nbimpaye")
+data <- data[,-c(idx_nbimpaye)]
+
+# We want to see what are the new variables
+colnames(model)
+# we note that nbpaiecb is generating a lot of dummies variables, but it's a quantitative value!
+# What is happpening is that there are missing values "." --> R sees these values as string and 
+# so considers that this attribute is qualitative 
+sum(data$nbpaiecb==".") # there are 278 indiv. with missing values at nbpaiecb
+# same for agemvt
+sum(data$agemvt==".") # there are 6 indiv. with missing values at agemvt
+
+# Solutions? 
+# 1.replace "." with NA and change columns to numeric
+# 2. do 1. and  impute values
+library('mice') 
+# first, we want to replace "." by NA
+nbpaiecb <- replace(data$nbpaiecb,which(data$nbpaiecb=="."),NA)
+data$nbpaiecb <- as.numeric(nbpaiecb)
+agemvt <- replace(data$agemvt,which(data$agemvt=="."),NA)
+data$agemvt <- as.numeric(agemvt)
+
+
+mice_mod <- mice(data, method = "rf")
+mice_output <- complete(mice_mod)
+
+# comparison
+idxes <- which(is.na(data$nbpaiecb))
+par(mfrow=c(2,2))
+hist(data$nbpaiecb, freq=F, main="nbpaiecb:Original Data", col="darkgreen", ylim=c(0,0.12))
+hist(mice_output$nbpaiecb, freq = F, main="nbpaiecb:Imputed Data", col="lightgreen", ylim=c(0,0.12))
+
+hist(data$agemvt, freq=F, main="nbpaiecb:Original Data", col="darkblue", ylim=c(0,0.12))
+hist(mice_output$agemvt, freq = F, main="nbpaiecb:Imputed Data", col="lightblue", ylim=c(0,0.12))
+
+data <-mice_output
+# now we build again the matrix :
+model <- model.matrix(~.+0, data=data)
+dim(model) # we are at 93, which is better but not enough!
+
+#### Latent class model ####
+# see article : latent class in the models, Iwona kasprzyk
+#for now, we just get rid f the department feature
+idx_departem <- which(colnames(data)=="departem")
+data <- data[,-c(idx_departem)]
+model <- model.matrix(~.+0, data=data)
+dim(model)
 
 #### Subset selection ####
-regfit.full <- regsubsets(cartevpr~., data, nvmax=20, really.big=T)
+regfit.full <- regsubsets(cartevpr~., model, nvmax=20)
 summary(regfit.full)
+
+#### Applying PCA ####
+# problem, some of features are not numeric: sitfamil, csp, codeqlt
+# we change data 
+idx_cartevpr <- which(colnames(data)=="cartevpr")
+log.data <- log(data[,-c(idx_cartevpr)])
+data.cartevpr <- data[, idx_cartevpr]
+
+# apply PCA - scale. = TRUE is highly 
+# advisable, but default is FALSE. 
+ir.pca <- prcomp(log.ir,
+                 center = TRUE,
+                 scale. = TRUE) 
+
+
